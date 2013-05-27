3c91affa0bb726b112016c05bef3bd8f92a2070a
Avi-ec2-avivarma <avinash.varma4464@gmail.com>
2013-05-27 12:28:22 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vindle::Application.config.secret_token = 'ee526527b6d2c486019a97ccaba56ac606106cf4043b800299fcfc58fe9dd8f9ab20c972926201b9c6297b11d4c056daaafcd125f855ec62b1a1d39913a3d7ec'
